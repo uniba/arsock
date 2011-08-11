0ec31b471fe782236a4a59cba2cea4ff22221d01
nulltask <seiya@uniba.jp>
2011-08-11 14:40:50 +0000

    Google Map を表示するようにしてみた

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,6 @@
 -function(window, docuemnt, $, undefined) {
-	var socket = io.connect('http://' + window.location.host);
+	var map,
+		socket = io.connect('http://' + window.location.host);
 	socket.on('news', function(data) {
 		console.log(data);
 		socket.emit('someevent', { my: 'data' });
@@ -15,6 +16,19 @@
 		});
 		socket.on('location', function(data) {
 			$ul.prepend($('<li>' + JSON.stringify(data) + '</li>'));
+			if (map) {
+				
+			}
 		});
+	});
+
+	google.maps.event.addDomListener(window, 'load', function() {
+		var div = document.getElementById('map_canvas'),
+			map = new google.maps.Map(div, {
+				zoom: 8,
+				center: new google.maps.LatLng(35.663411, 139.70502),
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				scaleControl: true
+			});
 	});
 }(window, document, jQuery);
