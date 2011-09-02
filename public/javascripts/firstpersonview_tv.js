b9e67ba9cb131f22fd927a3d4a4ef9f9dbd44763
Haruma Kikuchi <haruma@uniba.jp>
2011-09-02 13:05:24 +0200

    修正

--- public/javascripts/firstpersonview_tv.js
+++ public/javascripts/firstpersonview_tv.js
@@ -1,6 +1,6 @@
 -function(window, docuemnt, $, undefined) {
 	var map,
-		socket = io.connect('http://' + window.location.host);
+		socket = io.connect('http://realtimeweblog.in:3000');
 	
 	socket.on('news', function(data) {
 		// console.log(data);
