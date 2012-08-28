4503c6800bba4f8481d7b2e4c747630a233aa786
hitsujiwool <utatanenohibi@gmail.com>
2012-08-28 15:48:14 +0900

    fixed deploy target.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,19 @@
-set :user, "realtimeweblog.in"
+set :user, "ec2-user"
 set :application, "arsock"
 set :repository, "git@github.com:uniba/arsock.git"
 set :scm, :git
 set :branch, "master"
 set :scm_verbose, true
 set :deploy_to, "/home/#{user}/app/#{application}"
-set :deploy_via, :copy
+set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
+
+set :node_env, 'production'
 
 default_environment['NODE_ENV'] = 'production'
 
-role :web, "realtimeweblog.in"
-role :app, "realtimeweblog.in"
+role :web, "54.247.166.129"
+role :app, "54.247.166.129"
 
 namespace :deploy do
   task :start do
@@ -25,8 +27,12 @@ namespace :deploy do
   end
 end
 
-after "deploy:symlink", :roles => :app do
-  run "ln -svf #{shared_path}/archives #{current_path}/public/archives"
+after "deploy:create_symlink", :roles => :app do
+  run "ln -sfv #{shared_path}/auth.json #{current_path}/config"
   run "ln -svf #{shared_path}/node_modules #{current_path}/node_modules"
   run "cd #{current_path} && npm install"
+end
+
+after "deploy:setup", :roles => :app do
+  run "mkdir -p #{shared_path}/node_modules"
 end
