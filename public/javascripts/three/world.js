fb9c26d815e53c481c79bc93b3df280034c1db8e
Haruma Kikuchi <haruma@uniba.jp>
2012-08-29 08:36:09 +0900

    moving some codes to world.js from birdview.js

--- public/javascripts/three/world.js
+++ public/javascripts/three/world.js
@@ -12,11 +12,18 @@
   function World(stream, state, width, height) {
     EventEmitter.call(this);
 
-    var that = this;    
+    var that = this;
+    
+    var renderer
+      , camera
+      , scene
+      , controls
+      , linz
+      , grid;
+      
+    var jsonLoader = new THREE.JSONLoader();
 
-    var camera, renderer, scene, light;
-
-    this.persons = {};
+    this.people = {};
     this.rendering = false;
     this.state = state;
 
@@ -24,57 +31,91 @@
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
     
-    camera = new THREE.PerspectiveCamera(45, 2, 0.1, 10000);
-    camera.rotation.x = - 0.3;
-    camera.position.y = 300;
-    camera.position.z = 800;
+    // camera
+    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100000);
+    camera.position.x = - 5000;
+    camera.position.y = 2000;
+    camera.position.z = 0;
 
     scene = new THREE.Scene();
     scene.add(camera);
-
-    light = new THREE.PointLight(0xFFFFFF);
-    light.position.x = 10;
-    light.position.y = 500;
-    light.position.z = 1000;
-    scene.add(light);
-    
-    var lineMaterial, geometry, line, i;
-    lineMaterial = new THREE.LineBasicMaterial({ color: 0xDDDDDD });
-	geometry = new THREE.Geometry();
-	geometry.vertices.push(new THREE.Vector3(-8000, 0, 0));
-	geometry.vertices.push(new THREE.Vector3( 8000, 0, 0));
-	for (i = 0; i <= 160; i ++ ) {
-	  line = new THREE.Line(geometry, lineMaterial);
-	  line.position.z = (i * 100) - 8000;
-	  scene.add(line);
-	  line = new THREE.Line(geometry, lineMaterial);
-	  line.position.x = (i * 100) - 8000;
-	  line.rotation.y = 90 * Math.PI / 180;
-	  scene.add(line);
-	}
-  
-    this.controls = new THREE.TrackballControls(camera, renderer.domElement);
-
-    this.renderer = renderer;
-    this.camera = camera;
-    this.scene = scene;
+      
+    controls = new THREE.TrackballControls(camera, renderer.domElement);
 
     // ユーザごとのstreamを作成
     stream.on('connection', function(personStream) {
       var person = new Person(personStream, that.scene);
-      that.persons[personStream.id] = person;
+      that.people[personStream.id] = person;
       that.scene.add(person);
     });
 
     state.on('hide', function(id) {
-      that.persons[id].hide();
+      that.people[id].hide();
     });
 
     state.on('show', function(id) {
-      that.persons[id].show();
+      that.people[id].show();
     });
 
     stream.connect();
+    
+    // grid
+    var gridParticles = new THREE.Geometry()
+      , gridPMaterial = new THREE.ParticleBasicMaterial({color: 0x009900, size: 50});
+    
+    grid = new THREE.ParticleSystem(gridParticles, gridPMaterial);
+    
+    for (var i=-50000, il=100000; i<il; i+=2000) {
+      for (var j=-50000, jl=100000; j<jl; j+=2000) {     
+        var p = new THREE.Vector3(); 
+  
+        p.x = i;
+        p.z = j;
+              
+        gridParticles.vertices.push(p);
+      }
+    }
+
+    scene.add(grid);
+    
+    // ars electronica center
+    
+    linz = new THREE.Object3D();
+    
+    jsonLoader.load('assets/models/linz/json/arselectronicacenter.js', function(geometry){
+      var material = new THREE.MeshBasicMaterial({color: 0x009900, wireframe: true})
+        , line = new THREE.Mesh(geometry, material);
+
+      line.position.x = 3000;
+      line.position.z = 1500;
+      line.rotation.y = Math.PI * 0.66;
+      line.scale.x = 500;
+      line.scale.y = 500;
+      line.scale.z = 500;
+      
+      that.linz.add(line);
+    }); 
+
+    // donau river (line)
+    jsonLoader.load('assets/models/linz/json/donau.js', function(geometry){
+      var material = new THREE.LineBasicMaterial({color: 0x009900, lineWidth: 1.25})
+        , line = new THREE.Line(geometry, material);
+
+      line.scale.x = 10000;
+      line.scale.y = 10000;
+      line.scale.z = 10000;
+      
+      that.linz.add(line);
+    });
+    
+    scene.add(linz);
+    
+    this.renderer = renderer;
+    this.camera = camera;
+    this.scene = scene;
+    this.controls = controls;   
+    this.linz = linz;
+    this.grid = grid;
   };
   
   World.prototype = new EventEmitter();
@@ -95,8 +136,8 @@
 
     this.emit('beforerender');
 
-    Object.keys(this.persons).forEach(function(id) {
-      that.persons[id].update();
+    Object.keys(this.people).forEach(function(id) {
+      that.people[id].update();
     });
 
     this.state.tick();
