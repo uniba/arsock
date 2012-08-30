b8614ef4beb4b9605c520d4a433789ba4b6fc1aa
hitsujiwool <utatanenohibi@gmail.com>
2012-08-30 14:15:48 +0900

    set verticesNeedUpdate flag for every rendering

--- public/javascripts/three/person.js
+++ public/javascripts/three/person.js
@@ -15,20 +15,6 @@
         particles = new THREE.Geometry(),
         route  = new THREE.ParticleSystem(particles, new THREE.ParticleBasicMaterial({ color: 0xff6600, size: 40 }));
 
-    this.add(token);
-    this.add(route);
-        
-    // route
-    route.geometry.__dirtyVertices = true;
-    route.geometry.__dirtyElements = true;
-    route.dynamic = true;
-
-    for (var i=0, il=50000; i<il; i++) {
-      var p = new THREE.Vector3(0, 0, 0);
-      p.y = 1000000;      
-      route.geometry.vertices.push(p);
-    }
-
     stream.on('latest', function(data) {
       if (data.type === 'location') {
         that.counter++;
@@ -45,13 +31,22 @@
     stream.on('past', function(data) {
     });
 
+    this.add(token);
+    this.add(route);
+        
+    for (var i = 0, il = 50000; i<il; i++) {
+      var p = new THREE.Vector3(0, 0, 0);
+      p.y = 1000000;      
+      route.geometry.vertices.push(p);
+    }
+
     this.token = token;
     this.route = route;
     this.particles = particles;
     this.counter = 0;
 
-    // setup initial location (just a dummy!)
-    this.updateLocation(Math.random() * 1000 - 500, 0, Math.random() * 1000 - 500);
+    // plot initial location
+    this.updateLocation(0, 0, 0);
   }
 
   Person.prototype = new THREE.Object3D();
@@ -89,6 +84,7 @@
 
   // 標準のループ
   Person.prototype.update = function() {
+    this.route.geometry.verticesNeedUpdate = true;
     
     // creating dummy data
     /*
