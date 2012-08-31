9c2a5cbe6601fb8da0cd28f79514010321dd2bce
Haruma Kikuchi <haruma@uniba.jp>
2012-08-31 13:48:41 +0900

    test : measurement standard 200 meters

---
+++ public/birdview/assets/models/linz/json/measurement_200m.js
@@ -0,0 +1,49 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "generatedBy"   : "Blender 2.63 Exporter",
+        "vertices"      : 4,
+        "faces"         : 2,
+        "normals"       : 1,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 1,
+        "morphTargets"  : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "SVGMat.001",
+	"blending" : "NormalBlending",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.800000011920929],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"depthTest" : true,
+	"depthWrite" : true,
+	"shading" : "Lambert",
+	"specularCoef" : 50,
+	"transparency" : 1.0,
+	"transparent" : false,
+	"vertexColors" : false
+	}],
+
+    "vertices": [-0.350000,-0.000000,-0.005556,0.350001,-0.000000,-0.005556,0.350001,0.000000,0.005556,-0.350000,0.000000,0.005556],
+
+    "morphTargets": [],
+
+    "normals": [0.000000,1.000000,0.000000],
+
+    "colors": [],
+
+    "uvs": [[]],
+
+    "faces": [34,1,0,3,0,0,0,0,34,3,2,1,0,0,0,0]
+
+
+
+}
